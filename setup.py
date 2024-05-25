6fc5beadc727a83f1e08bc9ea78efc0707575992
HwaI12 <s2222061@stu.musashino-u.ac.jp>
2024-05-25 15:05:19 +0900

    feat: CSVファイルとExcelファイルの読み込みと指定したカラムへの一意のIDの割り当て機能を追加

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='csv_identifier',
-    version='0.1',
+    version='0.1.2',
     description='A package to read CSV and assign unique IDs to a specified column',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'pandas',
+        'openpyxl',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
